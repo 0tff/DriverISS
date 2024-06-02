d6bfbe50f44ac0ebc88dcc8419d0e1e9de14387d
0tff <162051924+0tff@users.noreply.github.com>
2024-06-02 21:09:56 +0200

    Update main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+// credits to roblox
+package main
+
+import (
+	log "github.com/hashicorp/go-hclog"
+
+	"github.com/hashicorp/nomad/plugins"
+)
+
+func main() {
+	// Serve the plugin
+	plugins.Serve(factory)
+}
+
+// factory returns a new instance of a nomad driver plugin
+func factory(log log.Logger) interface{} {
+	return iis.NewIISDriver(log)
+}
