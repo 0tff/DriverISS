c2a853b10b93f7ad38dd1ede270223fc3453d348
0tff <162051924+0tff@users.noreply.github.com>
2024-06-02 21:39:29 +0200

    Create iis_test.go

---
+++ iis/iis_test.go
@@ -0,0 +1,14 @@
+package iis
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestISS(t *testing.T) {
+	// This test is so short it doesn't need a joke to lighten the mood.
+	// However, if you're feeling down, just remember: debugging is like
+	// finding a needle in a haystack, but the needle is also made of hay.
+	assert.True(t, true, "This should always pass")
+}
